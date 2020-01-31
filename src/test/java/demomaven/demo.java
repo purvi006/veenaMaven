cc9bb08d7d0fb428c9b95bf1f774a604f96bf976
veena <ajayveena0125@gmail.com>
2020-01-31 12:32:58 +0530

    first commit

---
+++ src/test/java/demomaven/demo.java
@@ -0,0 +1,14 @@
+package demomaven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo 
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("hiiiiiiiiii........",true);
+	}
+
+}
